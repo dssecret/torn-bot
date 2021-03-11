58bacb35e727a8e1a99ccaeb79ded314d668ff0b
dssecret <dsstudios.spam@gmail.com>
2021-03-11 10:11:02 -0800

    Addition for testing on production

--- __init__.py
+++ __init__.py
@@ -170,6 +170,7 @@ async def on_command_error(ctx, error):
             await asyncio.sleep(30)
             await message.delete()
     else:
+        await ctx.send(error)
         raise error
 
 
